43dda92c9c4c879e156b816a1500095867165996
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-06-06 06:18:12 -0400

    Created README.md file and in initialized git

---
+++ katas.php
@@ -0,0 +1,23 @@
+<?php
+
+$lang = [ ["english", "Welcome"]
+  , ["czech", "Vitejte"]
+  , ["danish", "Velkomst"]
+  , ["dutch", "Welkom"]
+  , ["estonian", "Tere tulemast"]
+  , ["finnish", "Tervetuloa"]
+  , ["flemish", "Welgekomen"]
+  , ["french", "Bienvenue"]
+  , ["german", "Willkommen"]
+  , ["irish", "Failte"]
+  , ["italian", "Benvenuto"]
+  , ["latvian", "Gaidits"]
+  , ["lithuanian", "Laukiamas"]
+  , ["polish", "Witamy"]
+  , ["spanish", "Bienvenido"]
+  , ["swedish", "Valkommen"]
+  , ["welsh", "Croeso"]
+  ];
+
+  echo $lang[0][0];
+  
